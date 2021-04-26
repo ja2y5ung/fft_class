49ecd6c83b1be97d9e078f7eb8d731c3f6793619
Minjusik <49944621+Minjusik@users.noreply.github.com>
2021-04-26 18:04:26 +0900

    Update back.py

--- back.py
+++ back.py
@@ -1,4 +1,4 @@
-# 2021 04 22 Restart
+# 2021 04 26 그래프 복원 문제 해결, 구간 선택하는 부분 코딩
 import numpy as np
 from numpy import exp, pi, sin
 from matplotlib import pyplot as plt
@@ -13,27 +13,29 @@ class backend:
 
     file                  = 0 # 파일
     columnDataLength      = 0 # 파일의 열 길이
+    
     orgnlData             = 0 # 데이터
     dcData                = 0 # 데이터 오프셋
+    lngthData             = 0 # 데이터 길이
 
+    f = 0.2
+    T = 1/f
     
-
-    dataLngth             = 0 # 데이터 길이
     fftData               = 0 # 원 데이터를 푸리에 변환
     amplt                 = 0 # 원 데이터의 진폭 스펙트럼
     phase                 = 0 # 원 데이터의 위상 스펙트럼
 
-    intrvList             = [] #잘라낸 구간을 저장할 리스트
+    intrvlData             = [] #잘라낸 구간을 저장할 리스트
 
     result                = 0 # 가공한 데이터 저장
     fig                   = 0 # 그래프 저장할 객체
     fig2                  = 0 # 잘라낸 그래프 저장할 객체
+    fig3                  = 0 # 잘라낸 그래프 합성하기
     
     
 
 
     def __init__(self):
-##        self.run()
         pass
     
     def run(self):
@@ -41,138 +43,200 @@ class backend:
         self.slctData(1)
         self.initData()
 
-        self.slctBySize()
-        self.ifft()
-##        self.saveFig()
-        self.show()
-        
+        self.getOrgn()
+        self.getIntrvl([0,800,1000,1500])
+        self.synthetic()
 
+
+
+
+
+
+        
     # 파일 불러오기
     def loadFile(self, _path):
         self.file = np.genfromtxt( _path, delimiter = ',', dtype = float, encoding = 'UTF-8')
         self.columnDataLength = self.file.shape[1]
         self.row_name = np.genfromtxt( _path, delimiter = ',', dtype = str)[0]
 
+
+
+
+        
     # 파일 안에서 데이터 선택하기
     def slctData(self, _num):
         self.orgnlData = self.file[1:, _num]
 
+
+
+
+        
     # 변수 초기화
     def initData(self):
-        self.dataLngth = len( self.orgnlData )
+        self.lngthData = len( self.orgnlData )
         self.dcData = self.orgnlData.mean()
-        self.orgnlData = self.orgnlData - self.dcData
+        self.orgnlData = np.array([self.orgnlData - self.dcData]).T
+
+        hlfLngth = self.lngthData // 2
+        
+        self.fftData = np.fft.fft( self.orgnlData, axis = 0) / self.lngthData
+        self.amplt = 2 * abs( self.fftData[0:hlfLngth] )
+        self.phase = np.angle( self.fftData[0:hlfLngth], deg = False)
+
+
+
 
         
-        hlfLng = int( self.dataLngth / 2 )
-        
-        self.fftData = np.fft.fft( self.orgnlData )
-        self.amplt = 2 * abs( self.fftData[0:hlfLng] )
-        self.phase = np.angle( self.fftData[0:hlfLng], deg = False)
-
     # 진폭순으로 선택
     def slctBySize(self , _N = 2):
-        hlfLng = len( self.amplt )
-        self.result = self.fftData
-        idxSortAmp = self.amplt.argsort()
-        self.a = idxSortAmp
-        N = idxSortAmp[-_N]
-        
-        self.result[ abs(self.fftData) < abs(self.fftData[N]) ] = 0
+        lngth = len( self.orgnlData ) // 2
+##        0425 백업용.. 쓸지 모름
+##        hlfLng = len( self.amplt )
+##        self.result = self.fftData
+##        idxSortAmp = self.amplt.argsort()
+##        self.a = idxSortAmp
+##        N = idxSortAmp[-_N]
+##        
+##        self.result[ abs(self.fftData) < abs(self.fftData[N]) ] = 0
 
 
-    # 밴드 패스 필터
-    def bandPassFltr(self, _intrvList):
-        self.intrvList = []
-        intrvLstln = len( _intrvList )
+
+
+
+    # 구간 잘라내기
+    def getIntrvl(self, _value):
+        intrvl = []
+        lngth = len(_value)
 
         # start, end값
-        for i in range(0, intrvLstln, 2):
-            start = _intrvList[i]
-            end = _intrvList[i+1]
-            self.intrvList.append( self.orgnlData[start:end] )
+        for i in range(0, lngth, 2):
+            start = _value[i]
+            end = _value[i+1]
+            intrvl.append( self.orgnlData[start:end] )
+            index = i // 2
 
-        # 출력할 수 있게 끔 열 데이터로 변환    
-        for i in range(0, intrvLstln, 2):
-            index = int(i/2)
-            self.intrvList[index] = self.intrvList[index].reshape( len( self.intrvList[index]) ,1)
+            
 
+        Hz = np.linspace(0, self.f/2, self.lngthData//2)
+        time = np.linspace(0, self.lngthData * self.T, self.lngthData)      
         # 원데이터 출력
-        col = int(intrvLstln/2) + 1
+        col = (lngth//2) + 1
         self.fig2 = plt.figure()
         plt1 = self.fig2.add_subplot(col,1,1)
-        plt1.plot( self.orgnlData )
+        plt1.plot(time, self.orgnlData )
+        plt1.grid()
+        plt1.set_xlabel("time")
+        plt1.set_ylabel("x(t))")
+        plt1.set_title("Orignal")
 
         # 잘래낸 값 출력
-        temp = []
-        for i in range(0, intrvLstln, 2):
-            index = int(i/2)
-            temp.append( self.fig2.add_subplot( col, 1, index +2))
-            temp[index].plot( self.intrvList[index])
-        self.fig2.tight_layout()
+        grphLst = []
+        for i in range(0, lngth, 2):
+            index = i//2
+            start = _value[i]
+            end = _value[i+1]
+            xAxis = np.linspace(start, end, end-start)
+            grphLst.append( self.fig2.add_subplot( col, 1, index +2))
+            grphLst[index].plot(xAxis, intrvl[index] )
+            grphLst[index].set_ylabel("x(t)")
+            grphLst[index].set_xlabel("time")
+            grphLst[index].set_title(chr(65+index))
             
-##        plt.show()
-##        
-    # 푸리에 역변환
-    def ifft(self):
-        self.result = np.fft.ifft( self.result ) * self.dataLngth
-            
-    # 출력
-    def show(self):
-        
-        self.fig = plt.figure()
-        plt1 = self.fig.add_subplot(3,2,1)
-        plt3 = self.fig.add_subplot(3,2,3)
-        plt5 = self.fig.add_subplot(3,2,5)
+        self.intrvlData = intrvl
+        self.intrvl = _value
+        self.fig2.tight_layout()  
+        self.fig2.show()
 
-        plt2 = self.fig.add_subplot(3,2,2)
-        plt4 = self.fig.add_subplot(3,2,4)
-        plt6 = self.fig.add_subplot(3,2,6)
+
+
+
+        
+    # 선택된 구간 합성
+    def synthetic(self):
+        lngth = len( self.intrvlData )
+        fft = []
+        amplt = []
+        phase = []
+        self.fig3 = plt.figure()
+        
+
+
+        for i in range(0,lngth):
+            index = i
+            fft.append(np.fft.fft( self.intrvlData[index], axis = 0 ))
+            amplt.append( abs( fft[index][0: len(fft[index])//2] ))
+            phase.append( np.angle( fft[index][0: len(fft[index])//2 ], deg = False ))
+            start = self.intrvl[i*2]
+            end = self.intrvl[i*2+1]
+            xAxis = np.linspace(start,end,end-start)
+            
+            p = self.fig3.add_subplot(lngth,3,1+index*3)
+            p.plot(xAxis, self.intrvlData[index])
+            p.set_xlabel("time")
+            p.set_ylabel("x(t)")
+            p = self.fig3.add_subplot(lngth,3,2+index*3)
+            p.stem(amplt[index])
+            p.set_xlabel("Hz")
+            p.set_ylabel("∣X(f)∣")
+            p = self.fig3.add_subplot(lngth,3,3+index*3)
+            p.stem(phase[index])
+            p.set_xlabel("Hz")
+            p.set_ylabel("∠X(f)")
+
+
+        ## 함수 합성하는 부분 def로 만들어야 함 04 25
+            self.fig4 = plt.figure()
+            
+
+        
+
+        self.fig3.tight_layout()
+        self.fig3.show()
+            
+
+
+
+
+
+
+                                      
+    # 원 데이터, 진폭, 위상 
+    def getOrgn(self):
+        self.fig = plt.figure()
+        plt1 = self.fig.add_subplot(3,1,1)
+        plt2 = self.fig.add_subplot(3,1,2)
+        plt3 = self.fig.add_subplot(3,1,3)
+
+        Hz = np.linspace(0, self.f/2, self.lngthData//2)
+        time = np.linspace(0, self.lngthData * self.T, self.lngthData)
+
 
         # 원 데이터 출력
-        time = np.arange(0,len(self.orgnlData)*5,5)
         plt1.plot(time, self.orgnlData)
         plt1.grid()
         plt1.set_xlabel("time")
-        plt1.set_ylabel("y")
+        plt1.set_ylabel("x(t)")
+        plt1.set_title("Orignal")
+        
+        
         
         # 진폭 스펙트럼 출력
-        Hz = np.arange(0, 0.2/2, 0.1/(len( self.orgnlData )*0.5) )
-        plt3.stem(Hz,self.amplt)
-        plt3.grid()
-        plt3.set_xlabel("Hz")
-        plt3.set_ylabel("Amplitue")
+        plt2.stem(Hz,self.amplt)
+        plt2.grid()
+        plt2.set_xlabel("Hz")
+        plt2.set_ylabel("∣X(f)∣")
+        plt2.set_title("Amplitude")
         
         # 위상 스펙트럼 출력
+        plt3.stem(Hz, self.phase)
+        plt3.grid()
+        plt3.set_xlabel("Hz")
+        plt3.set_ylabel("∠X(f)")
+        plt3.set_title("Phase")
         
-        plt5.stem(Hz, self.phase)
-        plt5.grid()
-        plt5.set_xlabel("Hz")
-        plt5.set_ylabel("Phase")
-        
-        # 진폭으로 크기 순으로 2개 뽑아서 ifft한 그래프
-        plt2.plot(time, self.result)
-        plt2.set_ylabel("y")
-        plt2.set_xlabel("ifft")
-        plt2.grid()
 
-        # A*sin(wt-q)로 진폭 크기순 2개 뽑아서 만든 그래프 : 주기가 있는 연속 신호일 경우
-        t = np.arange(0,2500*5,5)
-        plt4.plot(t, self.amplt[1]*sin(2*pi*1*0.1/1250*t +1*0.1*self.phase[1] ) + \
-                 self.amplt[4]*sin(2*pi*4*0.1/1250*t  + 4*0.1*self.phase[4] ))
-        plt4.set_ylabel("y")
-        plt4.set_xlabel("A*Sin( 2*pi*f(t - q))")
-        plt4.grid()
-
-        # A*sin(wt-q)로 진폭 크기순 2개 뽑아서 만든 그래프 : 길이가 N인 이산 신호일 경우
-        plt6.plot(t, self.amplt[1]*sin(2*pi*1/2500*( t - self.phase[1] )) + \
-                 self.amplt[4]*sin(2*pi*4/2500*( t - self.phase[4] )) )
-        plt6.set_ylabel("y")
-        plt6.set_xlabel("A*Sin( (2*pi*k)/N(t - q) )")
-        plt6.grid()
-        
         self.fig.tight_layout()
-##        plt.show()
+        self.fig.show()
 
     
 
